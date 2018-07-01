2ae38646e1b689d9cb4bec992f761566037bd41f
meixuesong <meixuesong@gmail.com>
2018-07-01 15:40:47 +0800

    remove unused imports

--- src/main/java/cn/meixs/rocketmqdemo/SimpleConsumer.java
+++ src/main/java/cn/meixs/rocketmqdemo/SimpleConsumer.java
@@ -13,8 +13,6 @@ import org.springframework.util.Assert;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
